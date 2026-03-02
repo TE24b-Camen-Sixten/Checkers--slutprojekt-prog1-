1f933892e31f57b2485f24ded4881e75b09fc9fc
TE24b-Camen-Sixten <sixtencamen@icloud.com>
2026-03-02 09:41:39 +0100

    first commit

---
+++ Checkers (slutprojekt prog1)/Program.cs
@@ -0,0 +1,37 @@
+﻿static void PrintBoard()
+{
+    for (int y = 0; y < 8; y++)
+    {
+        for (int x = 0; x < 8; x++)
+        {
+            if (x % 2 == 0)
+            {
+                if (y % 2 == 0)
+                {
+                    Console.BackgroundColor = ConsoleColor.Gray;
+                }
+                else
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkGray;
+                }
+            }
+            else
+            {
+                if (y % 2 != 0)
+                {
+                    Console.BackgroundColor = ConsoleColor.Gray;
+                }
+                else
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkGray;
+                }
+            }
+
+            Console.Write(" ");
+        }
+        Console.WriteLine();
+    }
+}
+
+PrintBoard();
+Console.ReadLine();
