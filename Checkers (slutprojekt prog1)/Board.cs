43104fcc5892aec0459048b702a668363d0d4d2a
TE24b-Camen-Sixten <sixtencamen@icloud.com>
2026-03-03 15:00:34 +0100

    Lagt till två nya klasser och en struct. Ingen av dem är klar ännu.

---
+++ Checkers (slutprojekt prog1)/Board.cs
@@ -0,0 +1,9 @@
+class Board()
+{
+    public Piece[,] squares = new Piece[8,8]; // Gör ett 8x8 bräde som heter squares där varje ruta är en piece
+
+    void Initialize()
+    {
+        
+    }
+}
---
+++ Checkers (slutprojekt prog1)/Piece.cs
@@ -0,0 +1,15 @@
+class Piece()
+{
+    public (int, int) position;
+    public bool isBlack;
+    public bool isKing;
+
+    List<(int, int)> LegalMoves()
+    {
+        List<(int, int)> legalPositions = [];
+
+        //gör så den lägger in alla lagliga moves i listan
+        
+        return legalPositions;
+    }
+}
